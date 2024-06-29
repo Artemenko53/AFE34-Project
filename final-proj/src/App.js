53ac5fb638db4c69a36787540f08ca34ea9c2ff0
Artemenko53 <weresytr@outlook.com>
2024-06-29 21:01:01 +0300

    Small update
    Logo added

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -4,17 +4,24 @@ import jokeValue from "./joker";
 function App() {
   return (
     <div className="base">
-      <h1>Hello! Click on button to get a joke!</h1>
-      <input value={jokeValue} readOnly className="jokeInput"></input>
-      <button type="button" className="search">
-        Search a joke
-      </button>
-      <h4 className="legal">
-        ©️ Website created with{" "}
-        <a href="https://chucknorris.io" rel="noreferrer" target="_blank">
-          chucknorris.io
+      <header>
+        <a href="./App.js">
+          <svg
         </a>
-      </h4>
+      </header>
+      <main>
+        <h1>Hello! Click on button to get a joke!</h1>
+        <input value={jokeValue} readOnly className="jokeInput"></input>
+        <button type="button" className="search">
+          Search a joke
+        </button>
+        <h4 className="legal">
+          ©️ Website created with{" "}
+          <a href="https://chucknorris.io" rel="noreferrer" target="_blank">
+            chucknorris.io
+          </a>
+        </h4>
+      </main>
     </div>
   );
 }
