3726f9483ef65b154c2742dc0cfc50927513feb6
Artemenko53 <weresytr@outlook.com>
2024-07-13 14:40:28 +0300

    Added favicon
    Added working favicon

--- final-proj/src/error404.jsx
+++ final-proj/src/error404.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
     <>
       <p>You've got an error</p>
       <h1>404</h1>
-      <a href="./myPage.js">Return home</a>
+      <Link to="/">Return home</Link>
     </>
   );
 }
